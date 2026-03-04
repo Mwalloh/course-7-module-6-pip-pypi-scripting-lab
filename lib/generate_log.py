e03f94b659d4c110844ad651636b0f0c74f2851d
Jim Ochieng <jimochieng64@gmail.com>
2026-03-04 16:40:56 +0300

    Updated generate_log function

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+import requests
 
 def generate_log(data):
     # TODO: Implement log generation logic
@@ -9,11 +10,35 @@ def generate_log(data):
 
     # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
     # Hint: Use datetime.now().strftime("%Y%m%d")
-
+    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt".strip()
+    
     # STEP 3: Write the log entries to a file using File I/O
     # Use a with open() block and write each line from the data list
     # Example: file.write(f"{entry}\n")
 
     # STEP 4: Print a confirmation message with the filename
+    if os.path.exists(filename):
+        print("File already exists")
+        return
+    
+    elif type(data) == list:
+        with open(filename, "w")  as file:
+            for entry in data:
+                file.write(f"{entry}\n")
+        
+        print(f"Log written to {filename}")
+    else:
+        raise ValueError("Data should be a list.")
+        
+generate_log(["User logged in", "User updated profile", "Report exported"])
 
-    pass
+
+def fetch_data():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    if response.status_code == 200:
+        return response.json()
+    return {}
+
+if __name__ == "__main__":
+    post = fetch_data()
+    print(f"Fetched Post Title: {post.get('title', 'No title found')}")
