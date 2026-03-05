db63811ff502d5ee0bc08d5e2918e19b3791830c
Jim Ochieng <jimochieng64@gmail.com>
2026-03-05 21:44:19 +0300

    Made changes to pass tests

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import os
-import requests
 
 def generate_log(data):
     # TODO: Implement log generation logic
@@ -10,7 +9,7 @@ def generate_log(data):
 
     # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
     # Hint: Use datetime.now().strftime("%Y%m%d")
-    filename = f"lib/log_{datetime.now().strftime('%Y%m%d')}.txt".strip()
+    filename = f"lib/log_{datetime.now().strftime('%Y%m%d')}.txt"
     
     # STEP 3: Write the log entries to a file using File I/O
     # Use a with open() block and write each line from the data list
@@ -22,7 +21,7 @@ def generate_log(data):
         return
     
     elif type(data) == list:
-        with open(filename, "w")  as file:
+        with open(filename, "w") as file:
             for entry in data:
                 file.write(f"{entry}\n")
         
